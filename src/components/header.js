e9a55305b574d49c2ee29a2f34d46ad0295c0d9c
TWZ21 <tanweizhong15@hotmail.com>
2023-01-13 22:46:12 +0800

    wz commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,6 @@
-import React from 'react'
-import { Typography } from '@mui/material';
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Header() {
-  return (
-    <Typography 
-      variant = "h4">
-        He he XD
-    </Typography>
-  )
+  return <Typography variant="h4">He he XD</Typography>;
 }
