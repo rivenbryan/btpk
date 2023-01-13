aaded982124c475964968a3a6254fe8dfd0a8d67
TWZ21 <tanweizhong15@hotmail.com>
2023-01-13 22:40:35 +0800

    hehe

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Typography } from '@mui/material';
 
 export default function Header() {
   return (
-    <div>header</div>
+    <Typography 
+      variant = "h4">
+        He he XD 
+    </Typography>
   )
 }
