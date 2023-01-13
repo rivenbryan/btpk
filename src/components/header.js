8c0c4947f8996cce94a8540118972eb05b603e0e
Bryan <rivenbryan@gmail.com>
2023-01-13 22:41:39 +0800

    commit bryan changes

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
+import Typography from '@mui/material/Typography';
 export default function Header() {
   return (
-    <div>header</div>
+    <>
+     <Typography variant="h3" gutterBottom>
+        h1. Heading
+      </Typography>
+      </>
   )
 }
