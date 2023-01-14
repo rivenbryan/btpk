cdc9e98cd5388ad621bcc991941be5b7b8d9d6cd
TWZ21 <tanweizhong15@hotmail.com>
2023-01-14 12:10:40 +0800

    hi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import Header from './components/header'
-import Footer from './components/footer';
-import { useState } from "react";
+import Registration from './components/Registration/Registration'
 export default function App() {
   return (
     <>
-    <Header/>
-    <Footer/>
+    <Registration></Registration>
     </>
   )
 }
---
+++ src/components/Registration/Registration.js
@@ -0,0 +1,16 @@
+import { Card } from '@mui/material'
+import React from 'react'
+
+const Registration = () => {
+  return (
+    <>
+        <Card>
+            <Card.Body>
+
+            </Card.Body>
+        </Card>
+    </>
+  )
+}
+
+export default Registration
