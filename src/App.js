6de05fd0eca7c4f8b316703a78ecbef02bc094ae
TWZ21 <tanweizhong15@hotmail.com>
2023-01-14 12:12:58 +0800

    hehe

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Header from './components/header'
 import Footer from './components/footer';
 import { useState } from "react";
+import Registration from './components/Registration/Registration';
 export default function App() {
   return (
     <>
     <Header/>
     <Footer/>
+    <Registration/>
     </>
   )
 }
