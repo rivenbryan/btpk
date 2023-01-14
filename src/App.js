0dbb249dd8b510a33fd000276277bb223ea022c8
Bryan <rivenbryan@gmail.com>
2023-01-14 13:07:19 +0800

    commit landingPage

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Registration from './components/Registration/Registration';
 import MainPage from './components/mainPage/page';
 import LandingPage from './components/landingPage/landingPage';
 export default function App() {
-  const [user, setUser] = useState("Bryan")
+  const [user, setUser] = useState("")
 
   return (
     <>
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Typography } from '@mui/material';
 
 export default function LandingPage() {
   return (
-    <div>landingPage</div>
-  )
+    <Box sx={{textAlign: 'center', marginTop: 25, border: 1}}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Box sx={{}}>
+            <Typography>Team</Typography>
+          </Box>
+        </Grid>
+        <Grid item xs={6}>
+        <Box>
+            <Typography>Chatbot</Typography>
+          </Box>
+        </Grid>
+       
+      </Grid>
+    </Box>
+  );
 }
