235ba4ed9658e49ccd8454b30285df39bd672d63
Bryan <rivenbryan@gmail.com>
2023-01-15 10:35:53 +0800

    nm

--- src/Links.js
+++ src/Links.js
@@ -5,7 +5,6 @@ import LandingPage from "./components/landingPage/landingPage";
 import MainPage from "./components/mainPage/page";
 import LoginRegisterPage from "./components/LoginRegisterPage/LoginRegisterPage";
 import { UserContext } from "./context/userContext";
-
 export default function Links() {
   const [user, setUser] = useState("Bryan");
 
